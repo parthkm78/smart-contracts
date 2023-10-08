e2b1149bcddd649e3cd85eefc0426764502bbc74
Parth Mehta <51226907+parthkm78@users.noreply.github.com>
2023-10-08 15:01:45 +0530

    Create erc20_vault.sol

---
+++ erc20_vault.sol
@@ -0,0 +1,3 @@
+// Some amount of shares are minted when an user deposits.
+// The DeFi protocol would use the users' deposits to generate yield (somehow).
+// User burn shares to withdraw his tokens + yield.
